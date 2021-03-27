dd79aeeecd62e294b2dbd27d17da3ef36e2731d4
Prashal Tarkas <prashal09@gmail.com>
2021-03-28 00:34:16 +0530

    update

--- assets/typewrite.js
+++ assets/typewrite.js
@@ -52,6 +52,6 @@ window.onload = function () {
     // CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #ffffff}";
     document.body.appendChild(css);
 };
